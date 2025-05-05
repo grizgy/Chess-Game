7b45f831e7db996010e5441c32a04cbb61c307d6
Kristian Hristov <kristian_hristov@outlook.com>
2025-05-05 23:55:32 +0300

    implementing Movement control

--- src/App.tsx
+++ src/App.tsx
@@ -4,84 +4,95 @@ import WhiteQueen from './assets/whiteQueen.png'
 import WhiteRook from './assets/whiteRook.png'
 import WhiteBishop from './assets/whiteBishop.png'
 import WhiteKnight from './assets/whiteKnight.png'
-import whitePawn from './assets/whitePawn.png'
+import WhitePawn from './assets/whitePawn.png'
 import BlackKing from './assets/blackKing.png'
 import BlackQueen from './assets/blackQueen.png'
 import BlackRook from './assets/blackRook.png'
 import BlackBishop from './assets/blackBishop.png'
 import BlackKnight from './assets/blackKnight.png'
 import BlackPawn from './assets/blackPawn.png'
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 
+const rows = 8;
+const cols = 8;
+
+const mat = Array.from({ length: rows }, () => new Array(cols));
+
+const initialState : any []= []
+
+for (let i = mat.length-1; i >= 0; i--) {
+  for (let j = 0; j < mat[i].length; j++) {
+    const key = `${i}-${j}`;
+    
+    let value
+
+    if(i == 0 && (j == 0 || j == 7) ) {
+      value = WhiteRook
+    } else if (i == 0 && (j == 1 || j == 6) ) {
+      value = WhiteKnight
+    } else if (i == 0 && (j == 2 || j == 5)) {
+      value = WhiteBishop
+    } else if (i == 0 && (j == 3)) {
+      value = WhiteQueen
+    } else if (i == 0 && (j == 4)) {
+      value = WhiteKing
+    } else if(i == 1) {
+      value =  WhitePawn; 
+    } else if (i == 6) {
+      value = BlackPawn
+    } else if (i == 7 && (j == 0 || j == 7)) {
+      value = BlackRook
+    } else if (i == 7 && (j == 1 || j == 6) ) {
+      value = BlackKnight
+    } else if (i == 7 && (j == 2 || j == 5)) {
+      value = BlackBishop
+    } else if (i == 7 && (j == 3)) {
+      value = BlackQueen
+    } else if (i == 7 && (j == 4)) {
+      value = BlackKing
+    }
+
+
+    // Determine the class name based on the indices
+    const className = (i % 2 === j % 2) ? "grid-item black" : "grid-item white";
+
+    initialState.push(
+      <div key={key} className={className}>
+       {value ? <div className='chess-piece' style={{backgroundImage : `url(${value})`}}></div> : <></>} 
+        {/* {value} */}
+      </div>
+    );
+
+
+    
+  }
+}
 
 function App() {
+  
+    const [gridItems, setGridItems] = useState<any[]>(initialState)
+    const [gridX, setGridX] = useState(0)
+    const [gridY, setGridY] = useState(0)
+    const [activePiece, setActivePiece] = useState<HTMLElement | null>(null)
 
-    const rows = 8;
-    const cols = 8;
-
-    const mat = Array.from({ length: rows }, () => new Array(cols));
-
-    const renderGridItems = (mat : any) => {
-      const gridItems = []; // Array to hold the grid item elements
-    
-      for (let i = 0; i < mat.length; i++) {
-        for (let j = 0; j < mat[i].length; j++) {
-          const key = `${i}-${j}`;
-          
-          let value
-
-          if(i == 0 && (j == 0 || j == 7) ) {
-            value = BlackRook
-          } else if (i == 0 && (j == 1 || j == 6) ) {
-            value = BlackKnight
-          } else if (i == 0 && (j == 2 || j == 5)) {
-            value = BlackBishop
-          } else if (i == 0 && (j == 3)) {
-            value = BlackQueen
-          } else if (i == 0 && (j == 4)) {
-            value = BlackKing
-          } else if(i == 1) {
-            value =  BlackPawn; 
-          } else if (i == 6) {
-            value = whitePawn
-          } else if (i == 7 && (j == 0 || j == 7)) {
-            value = WhiteRook
-          } else if (i == 7 && (j == 1 || j == 6) ) {
-            value = WhiteKnight
-          } else if (i == 7 && (j == 2 || j == 5)) {
-            value = WhiteBishop
-          } else if (i == 7 && (j == 3)) {
-            value = WhiteQueen
-          } else if (i == 7 && (j == 4)) {
-            value = WhiteKing
-          }
-
-    
-          // Determine the class name based on the indices
-          const className = (i % 2 === j % 2) ? "grid-item black" : "grid-item white";
-    
-          gridItems.push(
-            <div key={key} className={className}>
-             {value ? <div className='chess-piece' style={{backgroundImage : `url(${value})`}}></div> : <></>} 
-              {/* {value} */}
-            </div>
-          );
-        }
-      }
-    
-      return gridItems; // Return the array of JSX elements
-    };
-
-
-    let activePiece : any = null; 
+      console.log("Rendering : ")
 
     
     const grabElement = (e : any) => {
 
       const element = e.target as HTMLElement;
+      const chessBoard = chessTable.current;
 
-      if(element.classList.contains('chess-piece')) {
+      if(element.classList.contains('chess-piece') && chessBoard) {
         console.log(element)
+
+        console.log( "setGridY :" + (Math.abs(Math.floor((e.clientY - chessBoard.offsetTop - 800)/100)) - 1))
+        console.log("setGridX :" + (Math.floor((e.clientX - chessBoard.offsetLeft)/100)))
+
+
+        setGridY(Math.abs(Math.floor((e.clientY - chessBoard.offsetTop - 800)/100)) - 1)
+        setGridX(Math.floor((e.clientX - chessBoard.offsetLeft)/100))
+        
 
         const x = e.clientX-50;
         const y = e.clientY-50;
@@ -89,7 +100,9 @@ function App() {
         element.style.left = `${x}px`;
         element.style.top = `${y}px`;
 
-        activePiece = element;
+        // activePiece = element;
+
+        setActivePiece(element)
   
       }
 
@@ -111,9 +124,6 @@ function App() {
         const x = e.clientX-50;
         const y = e.clientY-50;
         activePiece.style.position = 'absolute'
-        // activePiece.style.left = `${x}px`;
-        // activePiece.style.top = `${y}px`;
-  
 
         if(x < minX) {
           activePiece.style.left = `${minX}px`;
@@ -124,7 +134,6 @@ function App() {
         }
 
 
-
         if(y < minY) {
           activePiece.style.top = `${minY}px`;
         } else if (y > maxY) {
@@ -132,7 +141,6 @@ function App() {
         } else {
           activePiece.style.top = `${y}px`;
         }
-
 
       }
 
@@ -142,8 +150,72 @@ function App() {
 
     const leaveElement = (e : any) => {
 
-      if(activePiece) {
-        activePiece = null
+      console.log(e)
+      const chessBoard = chessTable.current;
+
+      if(activePiece && chessBoard) {
+
+        const y = Math.abs(Math.floor((e.clientY - chessBoard.offsetTop - 800)/100)) - 1;
+        const x = Math.floor((e.clientX - chessBoard.offsetLeft)/100);
+  
+        //new grid cell position
+        console.log(y,x)
+
+        setGridItems((value : any) => {
+
+          console.log(value)
+          const pieces = value.map((piece : any) => {
+            console.log(piece)
+
+
+            if(((piece.key.charAt(0)) == gridY) && ((piece.key.charAt(2)) == gridX)) {
+
+              //position to be removed from 
+              //(7 - Y) * 8 + X
+
+              console.log(piece.key.charAt(0)) //y value
+              console.log(piece.key.charAt(2)) //x value
+
+              const currentPosition = (7 - Number(piece.key.charAt(0)))* 8 + Number(piece.key.charAt(2))
+              // console.log(piece.props)
+              console.log(currentPosition)
+              // console.log(initialState[currentPosition].props)
+              const backgroundImage = initialState[currentPosition].props.children.props.style?.backgroundImage;
+              const currentClassname = initialState[currentPosition].props.className
+              // console.log(backgroundImage)
+
+
+              initialState[currentPosition] = 
+                <div key={piece.key} className={currentClassname}><></></div>
+              
+                console.log(initialState[currentPosition].props)
+
+              //position to be moved to
+              //(7 - Y) * 8 + X
+              // (y,x)
+
+              const newPosition = (7 - y)* 8 + x
+              console.log(newPosition)
+              console.log(value[currentPosition])
+
+              const className = ( x % 2 === y % 2  ) ? "grid-item black" : "grid-item white";
+              console.log(`${y}-${x}`)
+              initialState[newPosition] = 
+                <div key={`${y}-${x}`} className={className}>
+                  <div className='chess-piece' style={{backgroundImage : backgroundImage}}></div>
+                </div>
+
+
+            } 
+            
+
+            return piece;
+          })
+          return pieces;
+        })
+
+        setActivePiece(null)
+
       }
 
     }
@@ -156,7 +228,7 @@ function App() {
       onMouseUp={e => leaveElement(e)}
       ref={chessTable}
       className="grid-container">
-          {renderGridItems(mat)}
+          {gridItems}
       </div>
     </>
   )
